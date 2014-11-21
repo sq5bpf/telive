7f8279662a956ee33fa12f988beb2f11964884d8
sq5bpf <sq5bpf+github@lipkowski.org>
2014-11-21 22:31:11 +0100

    add telive.h

---
+++ telive.h
@@ -0,0 +1,11 @@
+enum tetra_mac_res_addr_type {
+	ADDR_TYPE_NULL  = 0,
+	ADDR_TYPE_SSI   = 1,
+	ADDR_TYPE_EVENT_LABEL   = 2,
+	ADDR_TYPE_USSI          = 3,
+	ADDR_TYPE_SMI           = 4,
+	ADDR_TYPE_SSI_EVENT     = 5,
+	ADDR_TYPE_SSI_USAGE     = 6,
+	ADDR_TYPE_SMI_EVENT     = 7,
+};
+
