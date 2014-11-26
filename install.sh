bda330819b610f850586ef865b67209b36fba725
sq5bpf <sq5bpf+github@lipkowski.org>
2014-11-26 18:46:16 +0100

    install script fix

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ if [ ! -d $T ]; then
 	echo "please make a $T directory, and make it writeable as user `whoami`"
 	exit
 fi
-mkdir $T/in $T/out $T/log $T/tmp
+mkdir $T/in $T/out $T/log $T/tmp $T/bin
 cp bin/* $T/bin
